5ed4b0569eaeca88899c993f472ef01f2e68d1ad
Brenden Evans <bhevans17@gmail.com>
2019-08-16 03:03:33 -0700

    "Finished quotemaker"

--- src/Quotemaker/Quotemaker.js
+++ src/Quotemaker/Quotemaker.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import "../sass/Quotemaker.scss";
+
 const quotesList = [
-  "quote 1",
-  "quote 2",
-  "quote 3",
-  "quote 4",
-  "quote 5",
-  "quote 6",
-  "quote 7",
-  "quote 8",
-  "quote 9",
-  "quote 10"
+  '"Children really brighten up a household. They never turn the lights off." - Ralph Bus',
+  '"Always remember that true beauty comes from within - from within bottles, jars, compacts, and tubes." - Peters Almanac  ',
+  '"Son, if you really want something in this life, you have to work for it. Now quiet! Theyre about to announce the lottery numbers." - Homer Simpson',
+  '"I always wanted to be somebody, but now I realize I should have been more specific." - Lily Tomlin',
+  '"I am not a vegetarian because I love animals; I am a vegetarian because I hate plants." - A. Whitney Brown',
+  '"What are the three words guaranteed to humiliate men everywhere? Hold my purse. - Unknown',
+  '"Giving birth is like taking your lower lip and forcing it over your head." - Carol Burnett',
+  '"If you cant see the bright side of life, polish the dull side." - Unknown',
+  '"On the keyboard of life, always keep one finger on the Escape key." - Unknown',
+  '"Dont worry about the world coming to an end today. It is already tomorrow in Australia." - Charles Schulz'
 ];
 
 let i = 0;
@@ -21,15 +23,7 @@ export default class Quotemaker extends React.Component {
     this.state = {
       currentQuote: quotesList[0]
     };
-    
-    this.prevQuote = this.prevQuote.bind(this);
     this.nextQuote = this.nextQuote.bind(this);
-  }
-
-  prevQuote() {
-    i = i - 1; // increase i by one
-    i = quotesList.length % i; // if we've gone too high, start from `0` again
-    return this.setState({ currentQuote: quotesList[i] }); // give us back the item of where we are now
   }
 
   nextQuote() {
@@ -43,11 +37,12 @@ export default class Quotemaker extends React.Component {
       <div class="quote-container">
         <div class="quote-wrapper">
           <div class="quotes">
-            <blockquote>{this.state.currentQuote}</blockquote>
+            <blockquote id="new-quote">{this.state.currentQuote}</blockquote>
           </div>
           <div class="button-wrapper">
-            <button onClick={this.prevQuote}>Previous</button>
-            <button onClick={this.nextQuote}>Next</button>
+            <a href="#" class="button is-primary" onClick={this.nextQuote}>
+              Next
+            </a>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //imports
-import Navbar from "./Navbar/Navbar.js";
 import Quotemaker from "./Quotemaker/Quotemaker.js";
 
 class App extends React.Component {
   render() {
     return (
       <div id="app">
-        <Navbar />
         <Quotemaker />
       </div>
     );
